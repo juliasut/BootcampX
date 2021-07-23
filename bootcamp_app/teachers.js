1ba62273f9b43dae98a04fe44179d91533c14829
juliasut <juliasutygina@gmail.com>
2021-07-22 20:21:55 -0600

    Change queries to parameterized

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query is a function that accepts an SQL query as a JavaScript string. Using the ` (backtick), we can write a multi line string like this to make our SQL look nicer. The function then returns a promise that contains our result when the query is successful.
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+WHERE cohorts.name LIKE '$1
 ORDER BY teacher;
-`)
+`;
+
+// store all potentially malicious values in an array.
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+// pool.query is a function that accepts an SQL query as a JavaScript string. Using the ` (backtick), we can write a multi line string like this to make our SQL look nicer. The function then returns a promise that contains our result when the query is successful.
+pool.query(queryString, values)
 // once the .then(res => {}) gets executed, we're not dealing with SQL or the database any more, we're just dealing with JavaScript objects.
 .then(res => {
   res.rows.forEach(row => {
